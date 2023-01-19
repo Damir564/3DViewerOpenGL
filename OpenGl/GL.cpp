7c0111681e0b530f9b6d16280b4352eb34178459
Damir564 <ddamir.nizamutdinov@yandex.ru>
2023-01-19 17:42:25 +0300

    first commit

---
+++ OpenGl/GL.cpp
@@ -0,0 +1,187 @@
+﻿#include "pch.h"
+#include "GL.h"
+
+
+COpenGL::COpenGL(void)
+{
+}
+
+
+COpenGL::~COpenGL(void)
+{
+    wglMakeCurrent(hDC, NULL);
+    wglDeleteContext(hRC);
+}
+
+
+void COpenGL::Init(void)
+{
+    // Âû ìîæåòå äîáàâèòü êîäû íàñòðîéêè àòðèáóòîâ ÷åðòåæà, òàêèå êàê "îáíàðóæåíèå îòêðûòîé ãëóáèíû"
+}
+
+
+bool COpenGL::SetupPixelFormat(HDC hdc)
+{
+    hDC = hdc;
+
+    PIXELFORMATDESCRIPTOR pfd =
+    {
+               sizeof(PIXELFORMATDESCRIPTOR), // ðàçìåð ñòðóêòóðû pfd
+               1, // íîìåð âåðñèè
+               PFD_DRAW_TO_WINDOW | // Ïîääåðæêà ðèñîâàíèÿ â îêíå
+               PFD_SUPPORT_OPENGL | // Ïîääåðæêà OpenGL
+               PFD_DOUBLEBUFFER, // Ðåæèì äâîéíîãî áóôåðà
+               PFD_TYPE_RGBA, // öâåòîâîé ðåæèì RGBA
+               24, // 24-áèòíàÿ ãëóáèíà öâåòà
+               0, 0, 0, 0, 0, 0, // Èãíîðèðîâàòü áèòû öâåòà
+               0, // Íåò êåøà íåïðîçðà÷íîñòè
+               0, // Èãíîðèðîâàòü áèòû ñäâèãà
+               0, // Íåò áóôåðà íàêîïëåíèÿ
+               0, 0, 0, 0, // Èãíîðèðîâàòü áèò íàêîïëåíèÿ
+               32, // áóôåð ãëóáèíû 32 áèòà   
+               0, // Áåç êåøà øàáëîíîâ
+               0, // Íåò âñïîìîãàòåëüíîãî êåøà
+               PFD_MAIN_PLANE, // Îñíîâíîé ýòàæ
+               0, // çàðåçåðâèðîâàíî
+               0, 0, 0 // Èãíîðèðîâàòü ñëîé, âèäèìîñòü è ìàñêó ïîâðåæäåíèé
+    };
+
+    int nPixelFormat; // Ôîðìàò ïèêñåëåé
+    if (!(nPixelFormat = ChoosePixelFormat(hDC, &pfd)))
+    {
+        MessageBox(NULL, L"can not find proper mode", L"Error", MB_OK | MB_ICONEXCLAMATION);
+        return FALSE;
+    }
+
+    SetPixelFormat(hDC, nPixelFormat, &pfd);
+    hRC = wglCreateContext(hDC); // Ñîçäàåì ñîîòâåòñòâóþùèé HGLRC, èñïîëüçóÿ HDC, èñïîëüçóåìûé ïðè ðèñîâàíèè GDI
+    wglMakeCurrent(hDC, hRC); // Ñäåëàòü HGLRC èñïîëüçóåìûì OpenGL äëÿ ðèñîâàíèÿ òåêóùåãî èíñòðóìåíòà ðèñîâàíèÿ
+
+    return TRUE;
+}
+
+
+void COpenGL::Render(COpenGlDoc *pDoc)
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    float fColor1[4], fColor2[4];
+    float par[4];
+    long i, j, k, n;
+
+    if (pDoc->m_Vert.size() == 0) return;
+
+    // Îïðåäåëÿåì ñâîéñòâà ìàòåðèàëà
+    static float mat_shininess[] = { 128.0f };
+    static float mat_specular[] = { 1.0f, 1.0f, 1.0f, 0.5f };
+    static float mat_diffuse[] = { 1.0f, 1.0f, 1.0f, 0.9f };
+    static float mat_ambient[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+
+
+    glScalef(pDoc->m_Scale, pDoc->m_Scale, pDoc->m_Scale);
+
+    glTranslatef(pDoc->trans.x, pDoc->trans.y, pDoc->trans.z);
+
+    glRotatef(pDoc->Rotation.x, 1, 0, 0);
+    glRotatef(pDoc->Rotation.y, 0, 1, 0);
+    glRotatef(pDoc->Rotation.z, 0, 0, 1);
+
+    glEnable(GL_NORMALIZE);
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT0 + 1);
+
+    static float ambient[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+    static float diffuse[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+    static float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+    static float position0[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+    static float position1[] = { 0.0f, 0.0f, 0.5f, 0.0f };
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, position0);
+
+   
+
+    glLightfv(GL_LIGHT0 + 1, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0 + 1, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0 + 1, GL_SPECULAR, specular);
+    glLightfv(GL_LIGHT0 + 1, GL_POSITION, position1);
+ 
+    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+    
+    glClearDepth(1.0f);
+
+    glDepthFunc(GL_LEQUAL);
+    glEnable(GL_DEPTH_TEST);
+
+  
+   glMatrixMode(GL_MODELVIEW);
+
+   
+    glBegin(GL_TRIANGLES);
+
+    //	Îïðåäåëÿåì öâåòà ëèöåâîé è îáðàòíîé ïîâåðõíîñòè
+    COLORREF color1 = RGB(100,0,0);
+    COLORREF color2 = RGB(127, 127, 127);
+
+    fColor1[0] = GetRValue(color1) / 255.f;
+    fColor1[1] = GetGValue(color1) / 255.f;
+    fColor1[2] = GetBValue(color1) / 255.f;
+    fColor1[3] = 1.0f;
+
+    fColor2[0] = GetRValue(color2) / 255.f;
+    fColor2[1] = GetGValue(color2) / 255.f;
+    fColor2[2] = GetBValue(color2) / 255.f;
+    fColor2[3] = 1.0f;
+
+    //		Óñòàíàâëèâàåì ñâîéñòâà ìàòåðèàëà ëèöåâîé ïîâåðõíîñòè
+    for (i = 0; i < 4; i++) par[i] = mat_diffuse[i] * fColor1[i];
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, par);
+
+    for (i = 0; i < 4; i++) par[i] = mat_ambient[i] * fColor1[i];
+    glMaterialfv(GL_FRONT, GL_AMBIENT, par);
+
+    //		Óñòàíàâëèâàåì ñâîéñòâà ìàòåðèàëà îáðàòíîé ïîâåðõíîñòè
+    for (i = 0; i < 4; i++) par[i] = mat_diffuse[i] * fColor2[i];
+    glMaterialfv(GL_BACK, GL_DIFFUSE, par);
+
+    for (i = 0; i < 4; i++) par[i] = mat_ambient[i] * fColor2[i];
+    glMaterialfv(GL_BACK, GL_AMBIENT, par);
+
+    //		Ðèñóåì ýëåìåíòû ïîâåðõíîñòè
+    for (int i = 0; i < pDoc->m_Vert.size(); i++)
+    {
+        vertex v = pDoc->m_Vert.at(i);
+        glNormal3f(v.v.x, v.v.y, v.v.z);
+        glVertex3f(v.p1.x, v.p1.y, v.p1.z);
+
+        glNormal3f(v.v.x, v.v.y, v.v.z);
+        glVertex3f(v.p2.x, v.p2.y, v.p2.z);
+
+        glNormal3f(v.v.x, v.v.y, v.v.z);
+        glVertex3f(v.p3.x, v.p3.y, v.p3.z);
+ 
+    }
+     
+    
+   
+    glEnd();
+
+  
+    SwapBuffers(hDC);
+}
+
+
+void COpenGL::Reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    
+}
---
+++ OpenGl/OpenGlView.cpp
@@ -0,0 +1,189 @@
+﻿
+// OpenGlView.cpp: реализация класса COpenGlView
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "OpenGl.h"
+#endif
+
+#include "OpenGlDoc.h"
+#include "OpenGlView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// COpenGlView
+
+IMPLEMENT_DYNCREATE(COpenGlView, CView)
+
+BEGIN_MESSAGE_MAP(COpenGlView, CView)
+	// Стандартные команды печати
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_SIZE()
+	ON_WM_VSCROLL()
+	ON_WM_MOUSEWHEEL()
+//	ON_WM_RBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+// Создание или уничтожение COpenGlView
+
+COpenGlView::COpenGlView() noexcept
+{
+	// TODO: добавьте код создания
+
+}
+
+COpenGlView::~COpenGlView()
+{
+}
+
+BOOL COpenGlView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: изменить класс Window или стили посредством изменения
+	//  CREATESTRUCT cs
+	//cs.style |= WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+	return CView::PreCreateWindow(cs);
+}
+
+// Рисование COpenGlView
+
+void COpenGlView::OnDraw(CDC* /*pDC*/)
+{
+	COpenGlDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: добавьте здесь код отрисовки для собственных данных
+	opengl.Render(pDoc);
+}
+
+
+// Печать COpenGlView
+
+BOOL COpenGlView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// подготовка по умолчанию
+	return DoPreparePrinting(pInfo);
+}
+
+void COpenGlView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте дополнительную инициализацию перед печатью
+}
+
+void COpenGlView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте очистку после печати
+}
+
+
+// Диагностика COpenGlView
+
+#ifdef _DEBUG
+void COpenGlView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void COpenGlView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+COpenGlDoc* COpenGlView::GetDocument() const // встроена неотлаженная версия
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(COpenGlDoc)));
+	return (COpenGlDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Обработчики сообщений COpenGlView
+
+
+void COpenGlView::OnInitialUpdate()
+{
+	CView::OnInitialUpdate();
+
+	// TODO: добавьте специализированный код или вызов базового класса
+	m_deltaX = 0; 
+	m_deltaY = 0;
+	opengl.SetupPixelFormat(::GetDC(GetSafeHwnd()));
+	opengl.Init();
+}
+
+
+void COpenGlView::OnSize(UINT nType, int cx, int cy)
+{
+	CView::OnSize(nType, cx, cy);
+
+	// TODO: добавьте свой код обработчика сообщений
+	if (cx == 0)
+	{
+		cx = 1;
+	}
+	opengl.Reshape(cx, cy);
+}
+
+
+void COpenGlView::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+	
+
+	CView::OnVScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+BOOL COpenGlView::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+	COpenGlDoc* pDoc = GetDocument();
+	
+	float  del = (abs(zDelta) / (100.f * zDelta));
+	pDoc->m_Scale += del;
+	if (pDoc->m_Scale <= 0)
+	{
+		pDoc->m_Scale -= del;
+	}
+	Invalidate(FALSE);
+	return CView::OnMouseWheel(nFlags, zDelta, pt);
+}
+
+void COpenGlView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	COpenGlDoc* pDoc = GetDocument();
+	CRect rect;
+	GetClientRect(&rect);
+	double Height = rect.Height();
+	double Width = rect.Width();
+	if (nFlags == 1)
+	{
+		m_deltaX = lastP.x - point.x;
+		m_deltaY = point.y - lastP.y;
+		pDoc->trans.x = pDoc->trans.x - (m_deltaX / 200.0);
+		pDoc->trans.y = pDoc->trans.y - (m_deltaY / 100.0);
+	}
+	if (nFlags == 2)
+	{
+		m_deltaX = point.x - lastP.x;
+		m_deltaY = point.y - lastP.y;
+		pDoc->Rotation.z += m_deltaX;
+		pDoc->Rotation.x -= m_deltaY;
+	}
+
+	lastP = point;
+
+	Invalidate(FALSE);
+	CView::OnMouseMove(nFlags, point);
+}
